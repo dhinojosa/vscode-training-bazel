d810689834d303c851536e694808ac730937f2cf
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2023-11-20 21:31:16 +0000

    Updated Tests and Build

--- src/test/java/com/xyzcorp/EmployeeTest.java
+++ src/test/java/com/xyzcorp/EmployeeTest.java
@@ -3,8 +3,9 @@ package com.xyzcorp;
 import org.junit.Test;
 import org.junit.Assert;
 
-public class EmployeeTest {
-    @Test public void example() {
-		Assert.assertEquals(1, 1);
+public class EmployeeTest extends Object {
+    @Test public void testFirstName() {
+		Employee employee = new Employee("Carl", "Withers");
+		Assert.assertEquals("Carl", employee.getFirstName());
 	}
 }
